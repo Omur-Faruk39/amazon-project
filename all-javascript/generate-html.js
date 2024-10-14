08b2662626ee743b5879c9d1575043d7ec9954d5
omur-faruk <demonff39bd@gmail.com>
2024-10-14 08:37:37 +0600

    interactive add cart btn and cart counter

--- all-javascript/generate-html.js
+++ all-javascript/generate-html.js
@@ -1,54 +1,71 @@
-import {products} from "../data/products.js";
+import { products } from "../data/products.js";
+import { cart } from "./cart.js";
 
 const mainTag = document.querySelector('main');
+const cartElm = document.querySelector('.cart-image-div > p');
 
+const cartCounter = () => {
+  cartElm.innerHTML = cart.length;
+};
 
-(() => {
-  for (let i = 0; i < products.length; i++) {
-    mainTag.innerHTML += `<div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image" src=${products[i].image}>
-      </div>
-
-      <div class="product-name limit-text-to-2-lines">
-        ${products[i].name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars" src="images/ratings/rating-${products[i].rating.stars * 10}.png">
-        <div class="product-rating-count">
-          ${products[i].rating.count}
+const renderProducts = () => {
+  products.forEach(product => {
+    mainTag.innerHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${product.image}">
         </div>
-      </div>
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+        <div class="product-rating-container">
+          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
+          <div class="product-rating-count">
+            ${product.rating.count}
+          </div>
+        </div>
+        <div class="product-price">
+          <span>$</span>${(product.priceCents / 100).toFixed(2)}
+        </div>
+        <div class="product-quantity-container">
+          <select class="js-quantity">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+          </select>
+        </div>
+        <div class="product-spacer"></div>
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+        <button class="js-add-btn" value="${product.id}">
+          Add to Cart
+        </button>
+      </div>`;
+  });
 
-      <div class="product-price">
-        <span>$</span>${(products[i].priceCents / 100).toFixed(2)}
-      </div>
+document.querySelectorAll('.js-add-btn').forEach(btn => {
+    btn.addEventListener("click", () => {
+      const select = btn.closest('.product-container').querySelector('.js-quantity');
+      if (Number(select.value) + cart.length < 10) {
+        for (let i = 0; i < select.value;i++) {
+          cart.push(btn.value);
+        }
+      } else {
+        alert('Cart is out of range')
+      }
+      cartCounter();
+    });
+  });
 
-      <div class="product-quantity-container">
-        <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-        </select>
-      </div>
+  cartCounter();
+};
 
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button>
-        Add to Cart
-      </button>
-    </div>`
-  }
-})();
+document.addEventListener('DOMContentLoaded', renderProducts);
